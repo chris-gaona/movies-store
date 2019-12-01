ae0facddad62531fb255396880cfa769bc99922b
Chris Gaona <chris@specialtacticssolutions.com>
2019-11-30 19:10:48 -0800

    Creates product page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
 import React from "react";
+import Link from "next/link";
 
-export default () => <div>Welcome to next.js!</div>;
+export default () => (
+  <div>
+    Welcome to next.js! <Link href="/product">Product</Link>
+  </div>
+);
