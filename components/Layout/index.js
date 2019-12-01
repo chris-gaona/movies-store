41d96e201bc21a5b401143f749082280c1e425aa
Chris Gaona <chris@specialtacticssolutions.com>
2019-11-30 19:33:18 -0800

    Modifies home page / index.js file

---
+++ components/Layout/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header";
+import Container from "./Container";
+import Content from "./Content";
+import Footer from "./Footer";
+
+export { Header, Container, Content, Footer };
